95fc9d1bce380de02a934b4b21191405794cc194
Rick Gibson <ricklesgibson@gmail.com>
2015-05-06 17:59:14 -0700

    New jQuery slideshow

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,88 +1,47 @@
-(function(){
+//.diy-slideshow (the divs that contain all of the figures)
+//.diy-slideshow.current figure (all slides inside current project)
+
+/**
+ * Changes the current project to the one that was clicked
+ */
+$(document).on("click", ".projectbutton", function(mouseEvent){
   
-var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.current figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
-    console.log($items);
-   
+    var projectId = $(mouseEvent.target).data('projectId');  
 
-var projectCounter = 0,
-    $projects = document.querySelectorAll('.diy-slideshow') // a collection of all the projects
-    numProjects = $projects.length;
-    console.log($projects);
-    $currentProject = document.querySelectorAll('.current');
+    //Switch the current project
+    $(".diy-slideshow").removeClass("current")
+    $("#" + projectId).addClass("current")
 
+    //Determine which figure to show
+    $(".diy-slideshow figure").removeClass("show")
+    $(".diy-slideshow.current figure:first-of-type").addClass("show")
 
-// this function is what cycles the slides, showing the next or previous slide and hiding all the others
-var showCurrent = function(){
-  var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
-  
-  // remove .show from whichever element currently has it 
-  // http://stackoverflow.com/a/16053538/2006057
-  [].forEach.call( $items, function(el){
-    el.classList.remove('show');
-  });
-  
-  // add .show to the one item that's supposed to have it
-  $items[itemToShow].classList.add('show');    
-};
+});
 
-var changeProject = function(){
-  var itemToShow = Math.abs(counter%numItems);
-  var projectToShow = Math.abs(projectCounter%numProjects);// uses remainder (aka modulo) operator to get the actual index of the element to show  
-  
-  [].forEach.call( $items, function(el){
-    el.classList.remove('show');
-  });
-  // remove .current from whichever element currently has it 
-  // http://stackoverflow.com/a/16053538/2006057
-  [].forEach.call( $projects, function(el){
-    el.classList.remove('current');
-  });
+$(document).on("click", ".prev", function(mouseEvent){
 
+    var $project = $(mouseEvent.target).parent();
+    var $item = $project.find(".show");
 
-  // add .current to the one item that's supposed to have it
-  $projects[projectToShow].classList.add('current');  
-  // $items[itemToShow].classList.add('show'); 
-  $items = document.querySelectorAll('.current figure');
-   $items[itemToShow].classList.add('show'); 
+    $item.removeClass("show");
+    if($item.prev("figure").length){
+      $item.prev("figure").addClass("show")
+    }else{
+        $project.find("figure").last().addClass("show")
+    }
 
-};
+});
+$(document).on("click", ".next", function(mouseEvent){
 
-// add click events to prev & next buttons 
-var nextSlide = document.querySelectorAll('.next');
-for (var i = 0; i < nextSlide.length; ++i){
-nextSlide[i].addEventListener('click', function() {
-     counter++;
-     showCurrent();
-  }, false);
-};
+    var $project = $(mouseEvent.target).parent();
+    var $item = $project.find(".show");
 
-var prevSlide = document.querySelectorAll('.prev');
-for (var i = 0; i < nextSlide.length; ++i){
-prevSlide[i].addEventListener('click', function() {
-     counter--;
-     showCurrent();
-  }, false);
-};
+    $item.removeClass("show");
+    if($item.next("figure").length){
+      $item.next("figure").addClass("show")
+    }else{
+        $project.find("figure").first().addClass("show")
+    }
 
-// wire up project buttons
-document.querySelector('#p1').addEventListener('click', function() {
-     projectCounter = 0;
-     changeProject();
-     
-  }, false);
+});
 
-document.querySelector('#p2').addEventListener('click', function() {
-     projectCounter = 1;
-     changeProject();
-     
-  }, false);
-
-document.querySelector('#p3').addEventListener('click', function() {
-     projectCounter = 2;
-     changeProject();
-     
-  }, false);
-  
-})();  
