fb5c5fa0d84a5b3506af2f190a012b11dd6a9d59
Rick Gibson <ricklesgibson@gmail.com>
2015-05-12 16:51:10 -0700

    Defer slideshow jquery to run after load

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,44 +4,47 @@
 /**
  * Changes the current project to the one that was clicked
  */
-$(document).on("click", ".projectbutton", function(mouseEvent){
-  
-    var projectId = $(mouseEvent.target).data('projectId');  
+$(document).ready(function(){
+    $(document).on("click", ".projectbutton", function(mouseEvent){
+      
+        var projectId = $(mouseEvent.target).data('projectId');  
 
-    //Switch the current project
-    $(".diy-slideshow").removeClass("current")
-    $("#" + projectId).addClass("current")
+        //Switch the current project
+        $(".diy-slideshow").removeClass("current")
+        $("#" + projectId).addClass("current")
 
-    //Determine which figure to show
-    $(".diy-slideshow figure").removeClass("show")
-    $(".diy-slideshow.current figure:first-of-type").addClass("show")
+        //Determine which figure to show
+        $(".diy-slideshow figure").removeClass("show")
+        $(".diy-slideshow.current figure:first-of-type").addClass("show")
 
+    });
+
+    $(document).on("click", ".prev", function(mouseEvent){
+
+        var $project = $(mouseEvent.target).parent();
+
+        console.log($project);
+        var $item = $project.find(".show");
+
+        $item.removeClass("show");
+        if($item.prev("figure").length){
+          $item.prev("figure").addClass("show")
+        }else{
+            $project.find("figure").last().addClass("show")
+        }
+
+    });
+    $(document).on("click", ".next", function(mouseEvent){
+
+        var $project = $(mouseEvent.target).parent();
+        var $item = $project.find(".show");
+
+        $item.removeClass("show");
+        if($item.next("figure").length){
+          $item.next("figure").addClass("show")
+        }else{
+            $project.find("figure").first().addClass("show")
+        }
+
+    });
 });
-
-$(document).on("click", ".prev", function(mouseEvent){
-
-    var $project = $(mouseEvent.target).parent();
-    var $item = $project.find(".show");
-
-    $item.removeClass("show");
-    if($item.prev("figure").length){
-      $item.prev("figure").addClass("show")
-    }else{
-        $project.find("figure").last().addClass("show")
-    }
-
-});
-$(document).on("click", ".next", function(mouseEvent){
-
-    var $project = $(mouseEvent.target).parent();
-    var $item = $project.find(".show");
-
-    $item.removeClass("show");
-    if($item.next("figure").length){
-      $item.next("figure").addClass("show")
-    }else{
-        $project.find("figure").first().addClass("show")
-    }
-
-});
-
