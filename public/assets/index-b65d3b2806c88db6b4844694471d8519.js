306264c633f047eced15f7c4804e6cee13fefa82
Rick Gibson <ricklesgibson@gmail.com>
2015-05-25 20:17:52 -0700

    asset precompile

---
+++ public/assets/index-b65d3b2806c88db6b4844694471d8519.js
@@ -0,0 +1,51 @@
+//.diy-slideshow (the divs that contain all of the figures)
+//.diy-slideshow.current figure (all slides inside current project)
+
+/**
+ * Changes the current project to the one that was clicked
+ */
+
+$(document).ready(function(){
+    $(document).on("click", ".projectbutton", function(mouseEvent){
+      
+        var projectId = $(mouseEvent.target).data('projectId');  
+
+        //Switch the current project
+        $(".diy-slideshow").removeClass("current")
+        $("#" + projectId).addClass("current")
+
+        //Determine which figure to show
+        $(".diy-slideshow figure").removeClass("show")
+        $(".diy-slideshow.current figure:first-of-type").addClass("show")
+
+    });
+
+    $(document).on("click", ".prev", function(mouseEvent){
+
+        var $project = $(mouseEvent.target).parent();
+
+        console.log($project);
+        var $item = $project.find(".show");
+
+        $item.removeClass("show");
+        if($item.prev("figure").length){
+          $item.prev("figure").addClass("show")
+        }else{
+            $project.find("figure").last().addClass("show")
+        }
+
+    });
+    $(document).on("click", ".next", function(mouseEvent){
+
+        var $project = $(mouseEvent.target).parent();
+        var $item = $project.find(".show");
+
+        $item.removeClass("show");
+        if($item.next("figure").length){
+          $item.next("figure").addClass("show")
+        }else{
+            $project.find("figure").first().addClass("show")
+        }
+
+    });
+});
